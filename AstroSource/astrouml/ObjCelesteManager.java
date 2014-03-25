24b8acead12047a1a916a3b6fa2c5d4a5b91d716
Vincent Stébé <vincent.stebe@gmail.com>
2014-03-25 16:08:55 +0100

    add dcc

---
+++ AstroSource/astrouml/ObjCelesteManager.java
@@ -0,0 +1,9 @@
+package astrouml;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class ObjCelesteManager {
+
+}
---
+++ AstroSource/astrouml/Univers.java
@@ -0,0 +1,35 @@
+package astrouml;
+
+import java.util.ArrayList;
+
+public class Univers {
+    /**
+     * @associates <{astrouml.Galaxie}>
+     */
+    private ArrayList galaxies;
+    
+    private Etoile soleil;
+    
+    private Galaxie galaxy;
+    private Etoile soleil;
+
+    public ArrayList<Galaxie> getAllGalaxies() {
+        return null;
+    } 
+    
+    public Galaxie getGalaxie(String nom) {
+        return null;
+    }
+
+    public Galaxie creerGalaxie(String nom, String type, int eloignement) {
+        return null;
+    }
+    
+    public ObjFroid creerObjetFroid(String nom, int rayonOrbite, int diametre, int periode, ObjFroid centreOrbite) {
+        return null;
+    }
+    
+    public Etoile creerEtoile(String nom, int magnitude, char age, Galaxie galaxie) {
+        return null;
+    }
+}
